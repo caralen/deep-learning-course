b4063bfda800f3095cba83cf9cde91dbefeaa5c0
caralen <alen.carin@gmail.com>
2020-04-18 14:20:02 +0200

    Task 3 - eval and show filters and loss

--- lab2/pt_conv.py
+++ lab2/pt_conv.py
@@ -1,5 +1,11 @@
+import os
 import time
+import math
+
 import numpy as np
+import matplotlib.pyplot as plt
+import skimage as ski
+import skimage.io
 
 from pathlib import Path
 
@@ -13,7 +19,7 @@ DATA_DIR = Path(__file__).parent / 'datasets' / 'MNIST'
 SAVE_DIR = Path(__file__).parent / 'out'
 
 config = {}
-config['max_epochs'] = 8
+config['max_epochs'] = 10
 config['batch_size'] = 50
 config['save_dir'] = SAVE_DIR
 config['weight_decay'] = 1e-3
@@ -32,7 +38,9 @@ class CovolutionalModel(nn.Module):
     self.fc1 = nn.Linear(32*7*7, fc1_width, bias=True)
     self.fc_logits = nn.Linear(fc1_width, class_count, bias=True)
 
-  def reset_parmeters(self):
+    self.reset_parameters()
+
+  def reset_parameters(self):
     for m in self.modules():
       if isinstance(m, nn.Conv2d):
         nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
@@ -53,13 +61,54 @@ class CovolutionalModel(nn.Module):
     h = self.fc1(h)
     h = torch.relu(h)
     logits = self.fc_logits(h)
-    # F.softmax(logits)
+    # logits = F.softmax(logits)
     return logits
+
+def draw_conv_filters(epoch, step, layer, save_dir):
+  w = layer.weight.data.detach().numpy()
+  num_filters = w.shape[0]
+  C = w.shape[1]
+  k = w.shape[2]
+  
+  w -= w.min()
+  w /= w.max()
+  border = 1
+  cols = 8
+  rows = math.ceil(num_filters / cols)
+  width = cols * k + (cols-1) * border
+  height = rows * k + (rows-1) * border
+
+  for i in range(C):
+    img = np.zeros([height, width])
+    for j in range(num_filters):
+      r = int(j / cols) * (k + border)
+      c = int(j % cols) * (k + border)
+      img[r:r+k,c:c+k] = w[j,i]
+    filename = 'torch_conv1_epoch_%02d_step_%06d_input_%03d.png' % (epoch, step, i)
+    ski.io.imsave(os.path.join(save_dir, filename), img)
+
+
+def save_show_loss(loss_list, save_dir):
+  loss_list
+  
+  filename = 'conv_losses.txt'
+  f=open(os.path.join(save_dir, filename), 'w')
+  for loss in loss_list:
+      f.write(str(loss)+'\n')
+  f.close()
+
+  plt.plot(loss_list, marker='o')
+  plt.title('Loss over epochs for CNN')
+  plt.xlabel('epoch')
+  plt.ylabel('loss')
+  plt.show()
+
 
 def train(model, train_x, train_y, valid_x, valid_y):
 
   lr_policy = config['lr_policy']
   batch_size = config['batch_size']
+  save_dir = config['save_dir']
   max_epochs = config['max_epochs']
   weight_decay = config['weight_decay']
 
@@ -69,11 +118,13 @@ def train(model, train_x, train_y, valid_x, valid_y):
   criterion = nn.CrossEntropyLoss()
   # optimizer = optim.SGD(model.parameters(), lr=param_delta, weight_decay=weight_decay)
 
-  # petlja učenja
+  losses = []
+
   for epoch in range(1, max_epochs+1):
 
     if epoch in lr_policy:
       optimizer = optim.SGD(model.parameters(), lr=lr_policy[epoch], weight_decay=weight_decay)
+    cnt_correct = 0.0
 
     permutation_idx = torch.randperm(num_examples)
     train_x = train_x[permutation_idx]
@@ -84,26 +135,52 @@ def train(model, train_x, train_y, valid_x, valid_y):
       batch_x = train_x[i*batch_size:(i+1)*batch_size, :]
       batch_y = train_y[i*batch_size:(i+1)*batch_size]
 
-      # Prolaz unaprijed
       logits = model.forward(batch_x)
-
-      # Dohvati gubitak
       loss = criterion(logits, batch_y)
-
-      # računanje gradijenata
       loss.backward()
-
-      # korak optimizacije
       optimizer.step()
-
-      # Postavljanje gradijenata na nulu
       optimizer.zero_grad()
 
-      if i % 5 == 0:
-        print(f'epoch {epoch}, step: {i*batch_size}/{num_examples}, batch loss = {loss}')
+      yp = logits.argmax(dim=1)
+      yt = batch_y
+      cnt_correct += (yp == yt).sum()
 
-def evaluate():
-  pass
+      if i % 5 == 0:
+        print("epoch %d, step %d/%d, batch loss = %.2f" % (epoch, i*batch_size, num_examples, loss))
+      if i % 100 == 0:
+        draw_conv_filters(epoch, i*batch_size, net.conv1, save_dir)
+      if i > 0 and i % 50 == 0:
+        print("Train accuracy = %.2f" % (cnt_correct / ((i+1)*batch_size) * 100))
+    
+    losses.append(loss.detach().numpy())
+    evaluate("Validation", valid_x, valid_y, net, criterion)
+  save_show_loss(losses, save_dir)
+
+def evaluate(name, x, y, net, criterion):
+
+  print("\nRunning evaluation: ", name)
+  batch_size = config['batch_size']
+  num_examples = x.shape[0]
+  assert num_examples % batch_size == 0
+  num_batches = num_examples // batch_size
+  cnt_correct = 0.0
+  loss_avg = 0.0
+
+  with torch.no_grad():
+    for i in range(num_batches):
+      batch_x = x[i*batch_size:(i+1)*batch_size, :]
+      batch_y = y[i*batch_size:(i+1)*batch_size]
+      logits = net.forward(batch_x)
+      yp = logits.argmax(dim=1)
+      yt = batch_y
+      cnt_correct += (yp == yt).sum()
+      loss_val = criterion(logits, batch_y)
+      loss_avg += loss_val
+
+  valid_acc = cnt_correct / num_examples * 100
+  loss_avg /= num_batches
+  print(name + " accuracy = %.2f" % valid_acc)
+  print(name + " avg loss = %.2f\n" % loss_avg)
 
 def dense_to_one_hot(y, class_count):
     return np.eye(class_count)[y]
@@ -131,5 +208,5 @@ if __name__ == "__main__":
   test_y = torch.from_numpy(test_y)
 
   net = CovolutionalModel(1, 16, 512, 10)
-  train(net, train_x.float(), train_y, valid_x, valid_y)
+  train(net, train_x.float(), train_y, valid_x.float(), valid_y)
 
